050b83a04f4cee9958d1a670835581bf76473c83
AileenMolot <aileenmolot@gmail.com>
2017-12-10 18:35:27 +0200

    Hometask 6 done

--- Molot_6/Program.cs
+++ Molot_6/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Molot_lib;
+
 
 namespace Molot_6
 {
@@ -10,6 +12,39 @@ namespace Molot_6
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Matrix matrix1 = new Matrix(4, 4);
+
+                for (int y = 0; y < matrix1.GetRowsCount(); y++)
+                    for (int x = 0; x < matrix1.GetColumnsCount(); x++)
+                        matrix1.SetData(y, x, x + y * 10);
+
+                Matrix matrix2 = new Matrix(4, 4);
+
+                for (int y = 0; y < matrix2.GetRowsCount(); y++)
+                    for (int x = 0; x < matrix2.GetColumnsCount(); x++)
+                        matrix2.SetData(y, x, x + y * 100);
+
+                Matrix matrix3 = matrix1 + matrix2;
+
+                Matrix matrix4 = matrix2 + matrix1;
+
+                Matrix matrix5 = matrix1 - matrix2;
+
+                Matrix matrix6 = matrix2 - matrix1;
+
+                Matrix matrix7 = matrix1 * matrix2;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Argument exception: {0}", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}",ex.Message);
+            }
+
         }
     }
 }
---
+++ Molot_lib/Matrix.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molot_lib
+{
+    public class Matrix
+    {
+        private int[,] _matrix;
+
+        public Matrix (int rows, int columns)
+        {
+            if (rows <= 0 || columns <= 0)
+                throw new ArgumentException("Matrix can not exsist with zero or negative rows and columns.");
+
+            this._matrix = new int[rows, columns];
+        }
+
+        public void SetData (int row, int column, int value)
+        {
+            this._matrix[row, column] = value;
+        }
+
+        public int GetData (int row, int column)
+        {
+            int value = this._matrix[row, column];
+            return value;
+        }
+
+        public int GetRowsCount ()
+        {
+            int rowsCount = this._matrix.GetLength(0);
+            return rowsCount;
+        }
+
+        public int GetColumnsCount ()
+        {
+            int columnsCount = this._matrix.GetLength(1);
+            return columnsCount;
+        }
+
+        public static Matrix operator + (Matrix leftOperand, Matrix rightOperand)
+        {
+            if (leftOperand.GetRowsCount() != rightOperand.GetRowsCount() && leftOperand.GetColumnsCount() != rightOperand.GetColumnsCount())
+                throw new ArgumentException("Matrixs should be equal sized for this operation.");
+
+            Matrix result = new Matrix(leftOperand.GetRowsCount(), leftOperand.GetColumnsCount());
+
+            for (int row = 0; row < result.GetRowsCount(); row++)
+                for (int col = 0; col < result.GetColumnsCount(); col++)
+                    result.SetData(row, col, leftOperand.GetData(row, col) + rightOperand.GetData(row, col));
+
+            return result;
+        }
+
+        public static Matrix operator - (Matrix minuend, Matrix subtrahend)
+        {
+            if (minuend.GetRowsCount() != subtrahend.GetRowsCount() && minuend.GetColumnsCount() != subtrahend.GetColumnsCount())
+                throw new ArgumentException("Matrixs should be equal sized for this operation.");
+
+            Matrix result = new Matrix(minuend.GetRowsCount(), minuend.GetColumnsCount());
+
+            for (int row = 0; row < result.GetRowsCount(); row++)
+                for (int col = 0; col < result.GetColumnsCount(); col++)
+                    result.SetData(row, col, minuend.GetData(row, col) - subtrahend.GetData(row, col));
+
+            return result;
+        }
+
+        public static Matrix operator * (Matrix multiplier, Matrix factor)
+        {
+            if (multiplier.GetColumnsCount() != factor.GetRowsCount())
+                throw new ArgumentException("Number of columns in multiplier should be equal to number of rows in factor.");
+
+            Matrix result = new Matrix(multiplier.GetRowsCount(), factor.GetColumnsCount());
+
+            for (int resultRow = 0; resultRow < result.GetRowsCount(); resultRow++)
+                for (int resultCol = 0; resultCol < result.GetColumnsCount(); resultCol++)
+                {
+                    int sum = 0;
+
+                    for (int i = 0; i<multiplier.GetColumnsCount(); i++)
+                    {
+                        sum = sum + (multiplier.GetData(resultRow, i) * factor.GetData(i, resultCol));
+                    }
+
+                    result.SetData(resultRow, resultCol, sum);
+                }
+            return result;
+        }
+
+    }
+}
